955249e3e526103cbbe69a7471e2e7d960800ce4
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-07-31 16:02:48 +0100

    updates informer step

--- client-go-informer/main.go
+++ client-go-informer/main.go
@@ -6,11 +6,11 @@ import (
 	"os"
 	"time"
 
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/tools/wait"
 )
 
 func main() {
@@ -18,16 +18,17 @@ func main() {
 }
 
 func watchpods() {
-	var nodes []string
 	kubeconfig := flag.String("kubeconfig", os.Getenv("HOME")+"/.kube/config", "path to the kubeconfig file to use")
 	flag.Parse()
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
-		return nodes, err
+		fmt.Println(fmt.Errorf("Can't build config from flags: %v", err))
+		os.Exit(1)
 	}
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		return nodes, err
+		fmt.Println(fmt.Errorf("Can't get config: %v", err))
+		os.Exit(1)
 	}
 	resyncinterval := time.Second * 30
 	informerFactory := informers.NewSharedInformerFactory(clientset, resyncinterval)
@@ -38,5 +39,14 @@ func watchpods() {
 		DeleteFunc: func(obj interface{}) { fmt.Printf("Deleted pod: %v", obj) },
 	})
 	go informerFactory.Start(wait.NeverStop)
-	// pod, err := podInformer.Lister().Pods()
+	for {
+		time.Sleep(time.Second * 10)
+		pod, err := podInformer.Lister().Pods("default").Get("webserver")
+		if err != nil {
+			fmt.Println(fmt.Errorf("Can't get pod updates: %v", err))
+			continue
+		}
+		fmt.Printf("%v", pod)
+	}
+
 }
